00ade0758c72c0c1bb2d2926509ac2c2ac75f550
abhinavpc4 <shuklaabhinav824@gmail.com>
2025-03-27 19:27:40 +0530

    not too much

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import HomeScreen from './components/Home/HomeScreen'
 
 
 function App() {
-
-  const link="https://www.iconfinder.com/icons/4102326/cloud_sun_sunny_weather_icon"
   return (
     <>
-      <HomeScreen cityName="New York" temperature={70} weather="Sunny" logo={<img src={link} alt="Weather Logo" />} />
+      <HomeScreen/>
     </>
   )
 }
